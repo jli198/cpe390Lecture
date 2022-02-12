93830fd471e8e910175048ef280c1b3e9a06a91f
jli198 <jli198@stevens.edu>
2022-02-12 16:37:00 -0500

    lec08/test.cc

---
+++ lec08/test.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int add(int a, int b) { 
+  return a + b;
+}
+
+int sub(int a, int b) { 
+  return a - b; 
+}
+
+int mul(int a, int b) { 
+  return a * b;
+}
+
+int divide(int a, int b) { 
+  return a / b;
+}
+
+int sub(int a, int b) { 
+  return a - b;
+}
+
+int mult_add(int a, int b, int c) { 
+  return a*b + c; 
+}
+
+int add_mult(int a, int b, int c) { 
+  return (a+b)* c;
+}
