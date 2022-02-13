0ef5b4afb475dd15901df5513c92e5c1d33ae74a
jli198 <jli198@stevens.edu>
2022-02-12 22:35:00 -0500

    test.cc

--- lec08/test.cc
+++ lec08/test.cc
@@ -17,14 +17,22 @@ int divide(int a, int b) {
   return a / b;
 }
 
-int sub(int a, int b) { 
-  return a - b;
-}
-
-int mult_add(int a, int b, int c) { 
+int mul_add(int a, int b, int c) { 
   return a*b + c; 
 }
 
-int add_mult(int a, int b, int c) { 
+int add_mul(int a, int b, int c) { 
   return (a+b)* c;
 }
+
+uint64_t add(uint64_t a, uint64_t b) { 
+  return a + b;
+}
+
+bool is_prime(uint64_t n) { 
+  for (uint64_t i = 2; i < n; i++) {
+    if (n % i == 0) 
+      return false;
+    return true;
+  }
+ }
