185a9d1e26423003271361e95939a5ef873d8217
jli198 <jli198@stevens.edu>
2022-02-12 23:35:19 -0500

    test.cc back up

--- lec08/test.cc
+++ lec08/test.cc
@@ -1,1 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int add(int a, int b) { 
+  return a + b; 
+}
+
+uint64_t add(uint64_t a, uint64_t b) { 
+  return a + b; 
+}
+
+int sub(int a, int b) { 
+  return a - b; 
+}
+
+int mul(int a, int b) { 
+  return a * b; 
+}
+
+int divide(int a, int b) { 
+return a / b;
+}
+
+int mul_add(int a, int b, int c) { 
+return a*b + c;
+}
+
+int add_mul(int a, int b, int c) { 
+return (a+b)*c;
+}
+
+bool is_prime(uint64_t n) {
+  for (uint64_t i =2; i < n; i++)
+    if (n % i == 0)
+      return false;
+  return true; 
+}
 
