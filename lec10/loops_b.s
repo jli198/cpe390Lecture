13900fb74ca85d89b7aa161625d7dbd005a8bb7e
jli198 <jli198@stevens.edu>
2022-02-24 16:50:56 -0500

    loop_b.s

--- lec10/loops_b.s
+++ lec10/loops_b.s
@@ -5,18 +5,10 @@ main:
 loop:
 
                 //add x0, x5, x6 x0 = x5 + x6
-  add x1, x0, x1 // sum = sum + x0
-  add x0, x0, 1 // immediate mode | x0 = x0 + 1;
-  cmp x0, 10
+  add x1, x0, x1 // sum = sum + x0 | sum = 1+2+3+4
+  cmp x0, 4
+  add x0, x0, 1 // immediate mode | x0 = x0 + 1; | x0 = 5
   bne loop
-
-loop2:
-
-loop3:
-
-
-
-
 
   mov w0, 0
   ret
