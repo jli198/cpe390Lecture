8b5090ff61d71b8a95ae66c55abea0bbf39143a5
jli198 <jli198@stevens.edu>
2022-03-02 14:53:14 -0500

    obj

--- lec10/loops_b.s
+++ lec10/loops_b.s
@@ -4,7 +4,7 @@ main:
   mov x0, 1
 loop:
 
-                //add x0, x5, x6 x0 = x5 + x6
+  add x0, x5, x6  //add x0, x5, x6 x0 = x5 + x6
   add x1, x0, x1 // sum = sum + x0 | sum = 1+2+3+4
   cmp x0, 4
   add x0, x0, 1 // immediate mode | x0 = x0 + 1; | x0 = 5
