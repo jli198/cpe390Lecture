3e026748a36cbc0d02829e3b399787274e06e204
jli198 <jli198@stevens.edu>
2022-03-10 15:09:41 -0500

    c++calling

--- lec13/c++callingconventions.cc
+++ lec13/c++callingconventions.cc
@@ -14,4 +14,16 @@ double h(uint64_t a, uint64_t b[], uint32_t len, double x);
 
 int main() {
   int y = f(3);
+
+  //call f;
+  
+  // w0, w1, w2, w3, w4, w5, w6, w7 // remainder pushed on stack
+
+  // big(1,2,3,4,5,6,7,8,9,9,9,0,0,90,6);
+
+  
+
+  /* push a;
+  push b;
+  call f; */
 }
