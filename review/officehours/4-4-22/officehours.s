70915086e2ba67242b500c56c1dc767bb3668e98
jli198 <jli198@stevens.edu>
2022-04-05 15:39:17 -0400

    office hrs update

--- review/officehours/4-4-22/officehours.s
+++ review/officehours/4-4-22/officehours.s
@@ -1,19 +1,21 @@
   // identify what is in each register
   .global main
 main:
-0x555550750: mov x0, #3 // x0=3, pc=0x555550750
+0x555550750: mov x0, #3 // x0=3, pc=0x555550754
 0x555550754: mov x1, #4
 0x555550758: stp lr, x0, [sp, #-16]!
-0x55555075C: bl f (0x555550768) // lr=0x555550760, pc=0x555550768
+0x55555075C: bl f (0x555550768) // lr=0x555550760, pc=0x555550768 | lr = pc, pc = f
 0x555550760: ldr lr, x0, [sp], #16 
 0x555550764: ret // back to caller
 0x555550768: add x0, x0, x1
-0x55555076C: stp lr, x0, [sp, #-16]!
+0x55555076C: stp lr, x0, [sp, #-16]! // sp=sp-16 write lr, x0
 0x555550770: bl g (0x55555077C) // lr=0x555550774, pc=0x55555077C
 0x555550774: ldr lr, x0, [sp], #16 // lr=0x555550760
 0x555550778: ret //pc=0x555550760
 0x55555077C: add x4, x5, x6
 0x555550780: ret //pc=lr=0x555550774
+
+  // pc always advances by 4
 
    // 5    5   5    6 bits
   add x0, x5, x6 lsl #63
