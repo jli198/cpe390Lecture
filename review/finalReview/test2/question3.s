87843b5475d07fe36e816a6f01960f05a7e43d14
jli198 <jli198@stevens.edu>
2022-05-11 12:34:14 -0400

    FINAL final review

--- review/finalReview/test2/question3.s
+++ review/finalReview/test2/question3.s
@@ -6,3 +6,16 @@
   int b[] = {6, 12};
   y = andall(b, 2); // y = 4
 */
+
+  // in x0 = address of array, x1 = length
+  mov w3, 0
+  mov w5, 0
+1:
+  ldr w2, x0, #4 // load x0 into w2 then autoincrement by 4
+  ldr w4, x0
+  and w5, w4, w2 
+  sub w1, w1, 0
+  cmp w1, 0
+  bne 1b
+  mov x0, w2 // x0 = w2
+  ret
